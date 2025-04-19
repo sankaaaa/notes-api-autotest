a9eacaf58c675233c28a63358abdff36d4e0771f
sankaaaa <olexa2404@gmail.com>
2025-04-19 18:25:14 +0300

    all tests passed

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Використовуємо PORT з середовища або за замовчуванням 3395
-  const port = process.env.PORT || 3395;
+  const port = 3395;
   await app.listen(port);
   console.log(`Server is running on http://localhost:${port}`);
 }
